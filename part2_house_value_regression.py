7c857654c00cbf1861d30f4b57f4f71871b500a3
Jaime Sabal <jsabalb@gmail.com>
2021-11-24 19:17:41 +0000

    Changed file so its ready to submit

--- part2_house_value_regression.py
+++ part2_house_value_regression.py
@@ -495,13 +495,13 @@ def example_main():
     x_train, x_val, y_train, y_val = train_test_split(x_train_val, y_train_val, test_size=0.25, random_state=42)
 
     # fit regressor
-    regressor = Regressor(x)
-    regressor.fit(x, y)
+    regressor = Regressor(x_train)
+    regressor.fit(x_train, y_train, x_val, y_val)
     #regressor.plot_losses(val=True) #plot losses
     #print("Final validation loss: ", regressor.val_losses[-1])
 
     #save regressor
-    save_regressor(regressor)
+    #save_regressor(regressor)
 
     #evaluate model on test data
     test_err = regressor.score(x_test, y_test)
